d8c3f0253601bfb14fb0ac5d3bee0ef1c591d40c
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-10 19:58:20 -0600

    Initial server code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const notes = require("./db/db.json");
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET request for notes
+app.get("/api/notes", (req, res) => {
+  res.status(200).json(notes);
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
