acfff44d135b3f43b1b04a2fd05365c836195791
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-11 17:47:53 -0600

    Developing server

--- server.js
+++ server.js
@@ -16,9 +16,35 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
 // GET request for notes
 app.get("/api/notes", (req, res) => {
   res.status(200).json(notes);
+});
+
+// POST request
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a review`);
+  console.info(`${req.body}`);
+  console.info(res.json(req.body));
+//    if (title && text) {
+//  const { title, text } = res.json(req.body);
+//  const newNote = {
+//     title,
+//     text,
+//  };
+//  const response = {
+//   status: 'success',
+//   body: newNote,
+//  };
+//  console.log(response);
+//  res.status(201).json(response);
+// } else {
+//   res.status(500).json('Error posting note')
+// }
 });
 
 app.listen(PORT, () =>
