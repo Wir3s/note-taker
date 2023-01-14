5d9736ef8fc021676c078409b7ce3e8a6594a82a
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-13 20:12:25 -0600

    Reformatting for /routes structure

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
 const express = require("express");
 const path = require("path");
 const uuid = require("uuid");
-const fs = require('fs');
+const fs = require("fs");
 
 const notes = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Middleware
-
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-// Get route for index.html
+// GET  route for homepage
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+// GET route for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// GET request for notes
+// GET request for notes - move this to notes.js in /routes
 app.get("/api/notes", (req, res) => {
   res.status(200).json(notes);
 });
 
 // Function for reading and appending
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
-};
+// const readAndAppend = (content, file) => {
+//   fs.readFile(file, 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       const parsedData = JSON.parse(data);
+//       parsedData.push(content);
+//       writeToFile(file, parsedData);
+//     }
+//   });
+// };
 
 // POST request
 app.post("/api/notes", (req, res) => {
@@ -50,15 +50,15 @@ app.post("/api/notes", (req, res) => {
   console.info(`${req.body}`);
   console.info(res.json(req.body));
 
-  const { title, text } = req.body;
-
-  if (req.body) {
-    const newJot = {
-      title,
-      text,
-      jot_id: uuid(),
-    };
-  }
+  // const { title, text } = req.body;
+  //
+  //   if (req.body) {
+  //     const newJot = {
+  //       title,
+  //       text,
+  //       jot_id: uuid(),
+  //     };
+  //   }
 });
 
 app.listen(PORT, () =>
