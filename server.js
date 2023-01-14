f526bb9e56bdaa33b94bc2665124d575a7876093
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-14 14:33:51 -0600

    Testing delete route

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
 // const uuid = require("uuid");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const util = require("util");
-
 
 // Should this be used? It doesn't work currently
 const notes = require("./db/db.json");
@@ -38,10 +37,9 @@ app.get("/api/notes", (req, res) =>
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 );
 
-
 // Function for reading and appending - this will move to helper folder
 const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
@@ -59,33 +57,44 @@ const writeToFile = (destination, content) =>
 
 // POST request - this will move to routes
 app.post("/api/notes", (req, res) => {
-
   // Destructing assignment for items in req.body
   const { title, text } = req.body;
 
   // If all required properties are present
-    if (title && text) {
-  // Variable for the object, a new note
-      const newJot = {
-        title,
-        text,
-        id: uuidv4(),
-      };
+  if (title && text) {
+    // Variable for the object, a new note
+    const newJot = {
+      title,
+      text,
+      id: uuidv4(),
+    };
 
-      readAndAppend(newJot, "./db/db.json")
+    readAndAppend(newJot, "./db/db.json");
 
-      const response = {
-        status: 'success',
-        body: newJot,
-      };
+    const response = {
+      status: "success",
+      body: newJot,
+    };
 
-      res.json(response);
-    } else {
-      res.json('Error in posting note');
-    }
+    res.json(response);
+  } else {
+    res.json("Error in posting note");
+  }
 });
 
+// DELETE route for notes
+app.delete("/api/notes", (req, res) => {
+  const noteId = req.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
 
+      writeToFile("./db/db.json", result);
+
+      res.json(`Note has been deleted`);
+    });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
