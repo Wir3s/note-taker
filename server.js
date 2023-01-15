376218587444d3347c242dbbc3a8a7bf42d9aab6
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-01-14 20:35:12 -0600

    Fixing...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
-const notesRouter = require("./routes/notes.js");
+const notesRouter = require("./routes/notes");
 const app = express();
 
 // Middleware
